384c240f8c4e775afca5af87691ab8dc74baa59e
Matei Dorobantu <matei@Mateis-MacBook-Pro.local>
2014-09-29 21:51:45 -0600

    Trying out the latest version oj Jasmine

---
+++ Scripts/conway/spec/ArraySpec.js
@@ -0,0 +1,16 @@
+describe("Player", function() {
+
+  beforeEach(function() {
+    player = new Player();
+    song = new Song();
+  });
+
+  it("should be able to play a Song", function() {
+    player.play(song);
+    expect(player.currentlyPlayingSong).toEqual(song);
+
+    //demonstrates use of custom matcher
+    expect(player).toBePlaying(song);
+  });
+
+});
